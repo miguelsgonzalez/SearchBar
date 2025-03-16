23eb60b24d16868b9055c1515495bfbdda720708
migue <migue@DESKTOP-POH8G6H>
2025-03-16 15:52:15 -0300

    Adicionar arquivos de projeto.

---
+++ SearchBar/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SearchBar
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SearchBar/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SearchBar;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
